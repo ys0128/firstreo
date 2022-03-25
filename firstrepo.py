0590dc9e8d4a0b0f0a2be4fad5b8f2e53becd615
jungch15 <58167392+jungch15@users.noreply.github.com>
2022-03-25 18:19:22 +0900

    Update firstrepo.py

--- firstrepo.py
+++ firstrepo.py
@@ -1,10 +1,4 @@
-print("Open SW lecture")
-
 # 202101475 컴퓨터공학부 박윤서
 
-#editttttt
-#3333
-
-#forked before ys0128 changed
-#changed after luckyseo forked
-
+print("Open SW lecture")
+test = [x**2 for x in range(3)]
